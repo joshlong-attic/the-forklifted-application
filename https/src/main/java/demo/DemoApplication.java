e08a755f4a91e1efbb7f0ec410c572904fc91f8c
Josh Long <josh@joshlong.com>
2015-07-21 17:39:55 -0600

    adding https

---
+++ https/src/main/java/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+@RestController
+class GreetingsRestController {
+    @RequestMapping("/hi/{name")
+    String hi(@PathVariable String name) {
+        return "Hi, " + name + "!";
+    }
+}
