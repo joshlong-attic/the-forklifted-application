26093f7ff9b1868773905fef7b53e3723424a7e2
Josh Long <josh@joshlong.com>
2015-07-21 17:25:27 -0600

    adding sessions example

---
+++ sessions/src/main/java/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+@RestController
+class HelloRestController {
+
+    @RequestMapping("/")
+    String uid(HttpSession session) {
+        UUID uid = (UUID) session.getAttribute("uid");
+        if (uid == null) {
+            uid = UUID.randomUUID();
+        }
+        session.setAttribute("uid", uid);
+        return uid.toString();
+    }
+
+}
