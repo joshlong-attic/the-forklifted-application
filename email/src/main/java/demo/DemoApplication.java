edb8005c282454d38e37858525c2a58d4e5d149e
Josh Long <josh@joshlong.com>
2015-07-21 21:27:32 -0600

    adding sendgrid

---
+++ email/src/main/java/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package demo;
+
+import com.sendgrid.SendGrid;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    @Bean
+    CommandLineRunner runner(SendGrid sendGrid) {
+        return args -> {
+            SendGrid.Email email = new SendGrid.Email();
+            email.setHtml("<hi>Hello Josh</h1>");
+            email.setTo(new String[]{"jlong@pivotal.io"});
+            email.setToName(new String[]{"Josh"});
+            SendGrid.Response send = sendGrid.send(email);
+            System.out.println("received " + send.getCode());
+        };
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
